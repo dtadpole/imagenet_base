c6883ecfa9612cb31962cb0a26f463879d3904da
D.Tadpole <gsk390@gmail.com>
2023-10-01 02:15:02 -0700

    tune

--- main.py
+++ main.py
@@ -44,12 +44,14 @@ parser.add_argument('-b', '--batch-size', default=256, type=int,
                     help='mini-batch size (default: 256), this is the total '
                          'batch size of all GPUs on the current node when '
                          'using Data Parallel or Distributed Data Parallel')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)',
+parser.add_argument('--beta1', default=0.9, type=float, metavar='B1',
+                    help='beta1')
+parser.add_argument('--beta2', default=0.999, type=float, metavar='B2',
+                    help='beta2')
+parser.add_argument('--wd', '--weight-decay', default=1e-3, type=float,
+                    metavar='W', help='weight decay (default: 1e-3)',
                     dest='weight_decay')
 parser.add_argument('-p', '--print-freq', default=10, type=int,
                     metavar='N', help='print frequency (default: 10)')
@@ -191,9 +193,13 @@ def main_worker(gpu, ngpus_per_node, args):
     # define loss function (criterion), optimizer, and learning rate scheduler
     criterion = nn.CrossEntropyLoss().to(device)
 
-    optimizer = torch.optim.SGD(model.parameters(), args.lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay)
+    # optimizer = torch.optim.SGD(model.parameters(), args.lr,
+    #                             momentum=args.momentum,
+    #                             weight_decay=args.weight_decay)
+    optimizer = torch.optim.ADAMW(model.parameters(), args.lr,
+                                  betas=(args.beta1, args.beta2),
+                                  weight_decay=args.weight_decay)
+
 
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
