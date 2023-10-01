bc76bcdaef10d4cd4f907112bf2d33292cf29b4d
D.Tadpole <gsk390@gmail.com>
2023-10-01 00:58:51 -0700

    tune

--- main.py
+++ main.py
@@ -299,8 +299,8 @@ def main_worker(gpu, ngpus_per_node, args):
 
 
 def train(train_loader, model, criterion, optimizer, epoch, device, args):
-    batch_time = AverageMeter('Time', ':5.2f')
-    data_time = AverageMeter('Data', ':5.2f')
+    batch_time = AverageMeter('Time', ':4.2f')
+    data_time = AverageMeter('Data', ':4.2f')
     losses = AverageMeter('Loss', ':.3e')
     top1 = AverageMeter('Acc@1', ':6.2f')
     top5 = AverageMeter('Acc@5', ':6.2f')
